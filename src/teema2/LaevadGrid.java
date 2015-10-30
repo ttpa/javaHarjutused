7636b3f4b9e88841c8bd69a3748dbaf2502029a4
Timo-Taavi Paemurru <tpaemurr@klass2-29-l.intra.itcollege.ee>
2015-10-30 15:08:14 +0200

    teema2 laevad

--- src/teema2/LaevadGrid.java
+++ src/teema2/LaevadGrid.java
@@ -5,16 +5,32 @@ package teema2;
  */
 public class LaevadGrid {
     static int[][] laud;
+
+    private static int[][] laud;
+    static int lauaLaius = 3;
+    static int lauaKorgus = 3;
+
     public static  void  main(String[] args) {
-        //genereeri laud
-        // kysi
-        // kontrolli
-        // kas on
-        // gameover
+        //genereeri laud();
+        //paiguta laevad();
+        //kysi kasutajalt();
+        //kysi tabamust();
+        //kasOnLaevuAlles();
+        //gameover();
 
     }
-    public static void  genereeriLaud() {
-        laud = new int[9][9];
+    public static void  paigutaLaevad() {
+        for int (i = 0; i < lauaKorgus; i++){
+            for (int j = 0; j < lauaLaius; j ++){
+                laud[i][j] = (int) Math.round(Math.random());
+            }
+            System.out.println( Arrays.toString(laud[i]));
+        }
 
     }
+    public static void genereerilaud() {
+        //System.out.println("Jah, käivitan");
+        laud = new int[lauaKorgus][lauaLaius];
+    }
+
 }
