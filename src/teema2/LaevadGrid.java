91433d038b7dcef7cec617d4a0b66ec887728342
Timo-Taavi Paemurru <tpaemurr@klass2-29-l.intra.itcollege.ee>
2015-10-30 15:04:32 +0200

    teema2

---
+++ src/teema2/LaevadGrid.java
@@ -0,0 +1,20 @@
+package teema2;
+
+/**
+ * Created by tpaemurr on 30.10.15.
+ */
+public class LaevadGrid {
+    static int[][] laud;
+    public static  void  main(String[] args) {
+        //genereeri laud
+        // kysi
+        // kontrolli
+        // kas on
+        // gameover
+
+    }
+    public static void  genereeriLaud() {
+        laud = new int[9][9];
+
+    }
+}
