b38eff3143742d551bd29da0433810753db83f4d
Timo-Taavi Paemurru <tpaemurr@klass2-29-l.intra.itcollege.ee>
2015-10-31 19:18:36 +0200

    teema2 +laevad_v4

--- src/teema2/Harjutus3_logisisse.java
+++ src/teema2/Harjutus3_logisisse.java
@@ -17,5 +17,26 @@ import javafx.stage.Stage;
  * 3. Ebaõnnestunud katse näitab kasutajale errorit
  * 4. Õnnestunud katse puhul vaheta pilt uue vastu (kasvõi roheline ring), .
  */
-public class Harjutus3_logisisse {
+public class Harjutus3_logisisse extends Application{
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        VBox vbox = new VBox();
+        Scene loginScene = new Scene(vbox);
+        primaryStage.setScene(loginScene);
+        primaryStage.show();
+
+        Label kasutajaLabel = new Label("Kasutaja");
+        TextField kasutajaInput = new TextField();
+
+        Label paroolLabel = new Label("Parool");
+        //TextField paroolInput = new TextField();
+        PasswordField paroolInput = new PasswordField();
+        Button loginNupp = new Button("Logi sisse");
+
+        vbox.getChildren().addAll(kasutajaLabel, kasutajaInput);
+        vbox.getChildren().addAll(paroolLabel, paroolInput, loginNupp);
+
+        loginNupp.setOnAction();
+
+    }
 }
