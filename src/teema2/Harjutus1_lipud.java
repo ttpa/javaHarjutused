b667159bb9a87421d0387ad410b9872590f16b97
Timo-Taavi Paemurru <tpaemurr@klass2-29-l.intra.itcollege.ee>
2015-10-31 18:40:46 +0200

    teema2 +laevad_v2

--- src/teema2/Harjutus1_lipud.java
+++ src/teema2/Harjutus1_lipud.java
@@ -1,8 +1,27 @@
 package teema2;
 
+
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
 /**
  * 1. Joonista eesti lipp kasutades kolme Rectangle
  * 2. Joonista Jaapani lipp kasutades Rectangle ja Circle
  */
-public class Harjutus1_lipud {
+public class Harjutus1_lipud extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Pane pane =new Pane();
+        Scene stseen = new Scene(pane);
+        primaryStage.setScene(stseen);
+        primaryStage.show();
+        Rectangle kast = new Rectangle(100, 50);
+        Rectangle kast_a = new Rectangle(200, 250);
+        pane.getChildren().add(kast);
+        pane.getChildren().add(kast_a);
+    }
 }
