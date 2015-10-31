4a706c24c6b102462dfbdd7a9c2c0244b1a286e6
Timo-Taavi Paemurru <tpaemurr@klass2-29-l.intra.itcollege.ee>
2015-10-31 19:03:53 +0200

    teema2 +laevad_v3

--- src/teema2/Harjutus1_lipud.java
+++ src/teema2/Harjutus1_lipud.java
@@ -5,7 +5,9 @@ package teema2;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 /**
@@ -15,13 +17,36 @@ import javafx.stage.Stage;
 public class Harjutus1_lipud extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Pane pane =new Pane();
-        Scene stseen = new Scene(pane);
+        jaapanilipp(primaryStage);
+
+
+
+    }
+
+
+    private void jaapanilipp(Stage primaryStage) {
+        Pane pane = new Pane();
+        Scene stseen = new Scene(pane, 700, 400);
+//        Scene stseen = new Scene(pane);
         primaryStage.setScene(stseen);
         primaryStage.show();
-        Rectangle kast = new Rectangle(100, 50);
-        Rectangle kast_a = new Rectangle(200, 250);
-        pane.getChildren().add(kast);
-        pane.getChildren().add(kast_a);
+
+        Rectangle kast_a = new Rectangle(0, 0, 700, 400);
+        kast_a.setFill(Color.BLACK);
+//        pane.getChildren().add(kast_a);
+
+        Rectangle kast = new Rectangle(50, 50, 600, 300);
+        kast.setFill(Color.WHITE);
+
+        Circle kast_b = new Circle(350, 200, 100);
+        kast_b.setFill(Color.RED);
+
+//        pane.getChildren().add(kast);
+
+//        Rectangle kast_a = new Rectangle(10, 10, 300, 200);
+//        Rectangle kast = new Rectangle(10, 10, 300, 200);
+
+
+        pane.getChildren().addAll(kast_a, kast, kast_b);
     }
 }
